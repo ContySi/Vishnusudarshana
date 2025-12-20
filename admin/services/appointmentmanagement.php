b4ab322bbbff3c444c03220aa3c3a90b487c528e
ContySi <134215985+ContySi@users.noreply.github.com>
2025-12-20 15:05:19 +0000

    Refactor appointment management page structure and remove unused HTML

--- admin/services/appointmentmanagement.php
+++ admin/services/appointmentmanagement.php
@@ -4,19 +4,20 @@
 
 include_once '../../header.php';
 include_once '../sidebar.php';
+    <?php
+    exit;
+}
+// ...existing code...
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Appointment Management</title>
-    <!-- CSS Includes -->
-    <link rel="stylesheet" href="../../assets/css/style.css">
-    <!-- Add any other CSS includes from index.php here -->
-</head>
-<body>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include_once '../../footer.php'; ?>
+    <!-- JS Includes -->
+    <script src="../../assets/js/language.js"></script>
+    <!-- Add any other JS includes from index.php here -->
+</body>
     <div class="main-content">
         <div class="container-fluid">
             <div class="row">
